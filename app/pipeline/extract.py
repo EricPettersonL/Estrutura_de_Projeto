7ff653440471eb6caed2bd640a2b986bd54aae1e
Eric Petterson <ericpl.ff@gmail.com>
2024-03-16 16:23:43 -0300

    Terminando o pipeline

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -8,9 +8,11 @@ import pandas as pd
 
 def extract_excel(input_folder):
     """
-    função para extrair dados de arquivos Excel.
+    função para extrair dados de arquivos Excel da pasta data/input.
 
-    type: input_folder: str
+    type: input_folder(str): Caminho da pasta de arquivos (data/input)
+    
+    return: todos os dados
     """
     files = glob.glob(os.path.join(input_folder, "*.xlsx"))
     if not files:
